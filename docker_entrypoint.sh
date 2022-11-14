9e1fc5321760b00bced88362229c0c6da012ae09
Mateo <macalvo@fi.uba.ar>
2022-11-14 05:32:30 -0300

    Fix port bindign

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sed -i -e "s/SERVICE_TRIPS_REMOTE_URL/$SERVICE_TRIPS_REMOTE_URL/" ./prometheus.yml
+sed -i -e "s/PORT/$PORT/" ./prometheus.yml
 /bin/prometheus --config.file=prometheus.yml
