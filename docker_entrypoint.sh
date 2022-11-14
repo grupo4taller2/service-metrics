072976846b48dbe45bfae161e6d206c9b6553fba
Mateo <macalvo@fi.uba.ar>
2022-11-14 05:53:24 -0300

    try fix

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,4 +2,4 @@
 sed -i -e "s/SERVICE_TRIPS_REMOTE_URL/$SERVICE_TRIPS_REMOTE_URL/" ./prometheus.yml
 sed -i -e "s/localhost:PORT/localhost:$PORT/" ./prometheus.yml
 cat prometheus.yml
-/bin/prometheus --config.file=prometheus.yml
+/bin/prometheus --config.file=prometheus.yml --web.listen-address=:$PORT
