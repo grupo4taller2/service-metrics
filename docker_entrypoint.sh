7dd87e86d4f922309167b61c327d415ad88c1580
Mateo <macalvo@fi.uba.ar>
2022-11-14 06:09:11 -0300

    Change delimiter

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i -e "s/SERVICE_TRIPS_REMOTE_URL/$SERVICE_TRIPS_REMOTE_URL/" ./prometheus.yml
+sed -i -e "s#SERVICE_TRIPS_REMOTE_URL#$SERVICE_TRIPS_REMOTE_URL#" ./prometheus.yml
 /bin/prometheus --config.file=prometheus.yml --web.listen-address=:$PORT
